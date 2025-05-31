8f49cf2dc9a86c36a1c29afd58c183d4b1d22114
Smitha I M <imsmitha66@gmail.com>
2025-05-31 14:49:36 +0530

    jenkins check4

--- tests/api_Sample.spec.js
+++ tests/api_Sample.spec.js
@@ -3,8 +3,6 @@ let BASEURL = 'https://restful-booker.herokuapp.com/booking/'
 
 test('auth token',async() => {
 
-    Console.log("Fetching Auth Token")
-
     const apiContext = await request.newContext();
     const loginResponse = await apiContext.post('https://restful-booker.herokuapp.com/auth', {
     data: {
